73b46efc84d5426f464ae9851d334ad65bf349a9
ntunyuserge <ntunyuserge@gmail.com>
2023-04-29 05:03:44 +0100

    Initial commit

---
+++ src/main/kotlin/com/serge/nsn/gadgetgallery/GadgetGalleryApplication.kt
@@ -0,0 +1,11 @@
+package com.serge.nsn.gadgetgallery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GadgetGalleryApplication
+
+fun main(args: Array<String>) {
+	runApplication<GadgetGalleryApplication>(*args)
+}
---
+++ src/test/kotlin/com/serge/nsn/gadgetgallery/GadgetGalleryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.serge.nsn.gadgetgallery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GadgetGalleryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
